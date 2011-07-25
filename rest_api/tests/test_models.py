35c1e218c27cedc181ed303b5410a67638d0e52d
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2011-07-25 20:00:12 -0300

    Add test to models.py

---
+++ rest_api/tests/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.db import IntegrityError
+
+from rest_api.models import Url
+
+class UrlTest(TestCase):
+    def test_create(self):
+        url = Url.objects.create(long_url='http://hakta.com')
+        url.save()
+
+        self.assertEqual(url.long_url, 'http://hakta.com')
+        self.assertEqual(url.key, '')
+
+    def test_unique(self):
+        qs = Url.objects.all()
+        qs.exclude()
+
+        url = Url.objects.create(long_url='http://hakta.com')
+        url.save()
+
+        with self.assertRaises(IntegrityError):
+            url = Url.objects.create(long_url='http://hakta.com')
