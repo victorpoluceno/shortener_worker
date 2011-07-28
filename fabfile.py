f5e2af5243c478855340887e74130173bd28bcb9
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2011-07-28 15:03:31 -0300

    Syncdb before run integration tests

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,9 @@
-from fabric.api import local, put, run, sudo
+from fabric.api import local
 
 def run_tests(settings="settings"):
     local("python manage.py test rest_api --settings=%s" % settings)
 
 def run_integration_tests(settings="settings_tests"):
+    local("python manage.py syncdb --noinput --settings=%s" % settings)
     local("export PYTHONPATH=. && export DJANGO_SETTINGS_MODULE=%s "\
             "&& python rest_api/tests/test_integration.py" % settings)
