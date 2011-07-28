d1419932cd6012aece61984c71e7cdbf9816316b
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2011-07-28 15:17:20 -0300

    Run celeryd before run integration_tests

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,13 @@
-from fabric.api import local
+from fabric.api import local, run, settings
 
-def run_tests(settings="settings"):
-    local("python manage.py test rest_api --settings=%s" % settings)
+def run_tests(settings_file="settings"):
+    local("python manage.py test rest_api --settings=%s" % settings_file)
 
-def run_integration_tests(settings="settings_tests"):
-    local("python manage.py syncdb --noinput --settings=%s" % settings)
+def run_integration_tests(settings_file="settings_tests"):
+    local("python manage.py syncdb --noinput --settings=%s" % settings_file)
+    with settings(warn_only=True):
+        if local("test -e celeryd.pid").failed:
+            local("python manage.py celeryd_detach --settings=%s" % settings_file)
+
     local("export PYTHONPATH=. && export DJANGO_SETTINGS_MODULE=%s "\
-            "&& python rest_api/tests/test_integration.py" % settings)
+            "&& python rest_api/tests/test_integration.py" % settings_file)
